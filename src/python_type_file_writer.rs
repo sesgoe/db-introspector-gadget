2a60d0557c32982d5edfadae626bd82862895e1f
Ses Goe <jsesgoe@gmail.com>
2023-08-22 13:19:13 -0400

    added tests for conversion stuff

--- src/python_type_file_writer.rs
+++ src/python_type_file_writer.rs
@@ -55,7 +55,7 @@ pub(crate) fn write_python_dicts_to_str(
             let requires_backwards_compat =
                 iter.any(|p| starts_with_number(p) || contains_space(p) || is_python_keyword(p));
 
-            if requires_backwards_compat || backwards_compat_forced {
+            if backwards_compat_forced || requires_backwards_compat {
                 dict.as_backwards_compat_typed_dict_class_str()
             } else {
                 dict.as_typed_dict_class_str()
@@ -67,4 +67,348 @@ pub(crate) fn write_python_dicts_to_str(
     result.push_str(python_dicts_str.as_str());
 
     result
+}
+
+#[cfg(test)]
+mod test {
+    use crate::python_types::PythonDataType;
+    use indoc::indoc;
+
+    use super::*;
+
+    #[test]
+    fn convert_definitios_to_single_dict_for_single_table() {
+        let table_column_definitions = vec![
+            TableColumnDefinition {
+                table_name: String::from("some_table"),
+                column_name: String::from("column_one"),
+                nullable: false,
+                data_type: String::from("varchar"),
+            },
+            TableColumnDefinition {
+                table_name: String::from("some_table"),
+                column_name: String::from("column_two"),
+                nullable: true,
+                data_type: String::from("varchar"),
+            },
+        ];
+
+        let result = convert_table_column_definitions_to_python_dicts(table_column_definitions);
+
+        let expected = vec![PythonTypedDict {
+            name: String::from("SomeTable"),
+            properties: vec![
+                PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                },
+                PythonDictProperty {
+                    name: String::from("column_two"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                },
+            ],
+        }];
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn convert_definitions_to_two_dicts_for_two_tables() {
+        let table_column_definitions = vec![
+            TableColumnDefinition {
+                table_name: String::from("some_other_table"),
+                column_name: String::from("column_one"),
+                nullable: true,
+                data_type: String::from("varchar"),
+            },
+            TableColumnDefinition {
+                table_name: String::from("some_table"),
+                column_name: String::from("column_one"),
+                nullable: false,
+                data_type: String::from("varchar"),
+            },
+        ];
+
+        let result = convert_table_column_definitions_to_python_dicts(table_column_definitions);
+
+        let expected = vec![
+            PythonTypedDict {
+                name: String::from("SomeOtherTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                }],
+            },
+            PythonTypedDict {
+                name: String::from("SomeTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                }],
+            },
+        ];
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn sorts_python_dict_vec_correctly() {
+        let table_column_definitions = vec![
+            TableColumnDefinition {
+                table_name: String::from("b_table"),
+                column_name: String::from("column_one"),
+                nullable: false,
+                data_type: String::from("varchar"),
+            },
+            TableColumnDefinition {
+                table_name: String::from("a_table"),
+                column_name: String::from("column_one"),
+                nullable: true,
+                data_type: String::from("varchar"),
+            },
+        ];
+
+        let result = convert_table_column_definitions_to_python_dicts(table_column_definitions);
+
+        let expected = vec![
+            PythonTypedDict {
+                name: String::from("ATable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                }],
+            },
+            PythonTypedDict {
+                name: String::from("BTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                }],
+            },
+        ];
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn writes_single_dict_to_string() {
+        let dict = vec![PythonTypedDict {
+            name: String::from("SomeTable"),
+            properties: vec![
+                PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                },
+                PythonDictProperty {
+                    name: String::from("column_two"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                },
+            ],
+        }];
+
+        let result = write_python_dicts_to_str(dict, false);
+        let expected = indoc! {"
+            import datetime
+            from typing import TypedDict, Any
+
+
+            class SomeTable(TypedDict):
+                column_one: str
+                column_two: str | None
+            "};
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn writes_multiple_dict_to_string() {
+        let dicts = vec![
+            PythonTypedDict {
+                name: String::from("ATable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                }],
+            },
+            PythonTypedDict {
+                name: String::from("BTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                }],
+            },
+        ];
+
+        let result = write_python_dicts_to_str(dicts, false);
+        let expected = indoc! {"
+            import datetime
+            from typing import TypedDict, Any
+
+
+            class ATable(TypedDict):
+                column_one: str | None
+
+
+            class BTable(TypedDict):
+                column_one: str
+        "};
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn ignores_writing_dict_with_dollar_sign_in_name() {
+        let dicts = vec![
+            PythonTypedDict {
+                name: String::from("ATable$"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                }],
+            },
+            PythonTypedDict {
+                name: String::from("BTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                }],
+            },
+        ];
+
+        let result = write_python_dicts_to_str(dicts, false);
+        let expected = indoc! {"
+            import datetime
+            from typing import TypedDict, Any
+
+
+            class BTable(TypedDict):
+                column_one: str
+            "};
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn ignores_writing_dict_starts_with_number() {
+        let dicts = vec![
+            PythonTypedDict {
+                name: String::from("1Table"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                }],
+            },
+            PythonTypedDict {
+                name: String::from("BTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                }],
+            },
+        ];
+
+        let result = write_python_dicts_to_str(dicts, false);
+        let expected = indoc! {"
+            import datetime
+            from typing import TypedDict, Any
+
+
+            class BTable(TypedDict):
+                column_one: str
+        "};
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn first_dict_backwards_compat() {
+        let dicts = vec![
+            PythonTypedDict {
+                name: String::from("ATable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("1column"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                }],
+            },
+            PythonTypedDict {
+                name: String::from("BTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                }],
+            },
+        ];
+
+        let result = write_python_dicts_to_str(dicts, false);
+
+        let expected = indoc! {"
+            import datetime
+            from typing import TypedDict, Any
+
+
+            ATable = TypedDict('ATable', {
+                '1column': str | None,
+            })
+
+            class BTable(TypedDict):
+                column_one: str
+            "};
+
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn backwards_compat_forced_true() {
+        let dicts = vec![
+            PythonTypedDict {
+                name: String::from("ATable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("1column"),
+                    nullable: true,
+                    data_type: PythonDataType::String,
+                }],
+            },
+            PythonTypedDict {
+                name: String::from("BTable"),
+                properties: vec![PythonDictProperty {
+                    name: String::from("column_one"),
+                    nullable: false,
+                    data_type: PythonDataType::String,
+                }],
+            },
+        ];
+
+        let result = write_python_dicts_to_str(dicts, true);
+
+        let expected = indoc! {"
+            import datetime
+            from typing import TypedDict, Any
+
+
+            ATable = TypedDict('ATable', {
+                '1column': str | None,
+            })
+
+            BTable = TypedDict('BTable', {
+                'column_one': str,
+            })"};
+
+        assert_eq!(result, expected)
+    }
 }
